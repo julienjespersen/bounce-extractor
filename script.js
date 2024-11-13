99aecf8f7f6672e222f6481aacbebb0e3f21a643
Julien Jespersen <julien.jespersen@unige.ch>
2024-11-13 18:04:20 +0100

    feat: multi files

--- script.js
+++ script.js
@@ -12,25 +12,70 @@ function removeDuplicates(array) {
 }
 
 
-fileInput.addEventListener('change', function(e) {
-  const file = e.target.files[0];
-  const reader = new FileReader();
-  
-  reader.onload = function(e) {
-    const contents = e.target.result;
-    if (contents) {
-        let emails = extractEmail(contents);
-        if (emails) {
-            
-            finalList = [...finalList, ...emails];
-            finalList = removeDuplicates(finalList);
-            newText = finalList.join('\n')
-            pre.textContent = newText
-        }
-        
-        console.table(finalList);
-    }
-  };
-  
-  reader.readAsText(file);
+
+fileInput.addEventListener('change', function(event) {
+    const files = event.target.files; // or use fileInput.files
+    processFiles(files);
 });
+  
+function processFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const contents = e.target.result;
+            console.log('File contents:', contents);
+
+
+            if (contents) {
+                let emails = extractEmail(contents);
+                if (emails) {
+                    
+                    finalList = [...finalList, ...emails];
+                    finalList = removeDuplicates(finalList);
+                    newText = finalList.join('\n')
+                    pre.textContent = newText
+                }
+                
+                console.table(finalList);
+            }
+
+
+        };
+        reader.readAsText(file); // or readAsDataURL(file) for images
+    }
+}
+
+
+
+// fileInput.addEventListener('change', e => {
+
+//     // console.table(e.target.files)
+//     [e.target.files].forEach(file => {
+//         console.log(file)
+        
+
+
+//         // const file = e.target.files[0];
+//         let reader = new FileReader();
+        
+//         reader.onload = function(ev) {
+//             let contents = ev.target.result;
+//             if (contents) {
+//                 let emails = extractEmail(contents);
+//                 if (emails) {
+                    
+//                     finalList = [...finalList, ...emails];
+//                     finalList = removeDuplicates(finalList);
+//                     newText = finalList.join('\n')
+//                     pre.textContent = newText
+//                 }
+                
+//                 console.table(finalList);
+//             }
+//         };
+        
+//         reader.readAsText(file);
+//     });
+
+// });
