b233410a822e1c4c8cbf2c0e906bd310044b2f8c
Julien Jespersen <julien.jespersen@unige.ch>
2024-11-20 17:50:51 +0100

    add css and cleanup

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const pre = document.querySelector('pre')
 const tmlTableRow = document.getElementById('tml-table-row')
 const outTable = document.getElementById('out-table')
 
-// const cleanup = document.querySelector('#cleanup');
 const dlBtn = document.querySelector('#download')
-var emails = []
 var emails2 = []
 
 function extractEmail(text) {
@@ -16,22 +14,18 @@ function extractEmail(text) {
     } else {
         return false
     }
-    // return text.match(emailRegex)
 }
 function extractEmail2(text) {
     const emailRegex = /^To:.*$/m
-    return extractEmail(text.match(emailRegex).toString()) //.toString().substring(3)
+    return extractEmail(text.match(emailRegex).toString())
 }
 
 function removeDuplicates(array) {
     return [...new Set(array)];
 }
 
-
-
 function myCleanup(anEmail) {
     parts = anEmail.split(/[@.]/)
-    // console.log(parts)
     if (parts[0].length > 31) {
         return true
     } else {
@@ -46,7 +40,6 @@ function addToTable(res, file) {
     outTable.querySelector('tbody').appendChild(node)
     
 }
-
 
 function downloadCSV(data, filename = 'export.csv') {
     // Convert array to CSV string
@@ -76,20 +69,9 @@ function processFiles(files) {
         reader.onload = function(e) {
             const contents = e.target.result;
             if (contents) {
-                // let emails = extractEmail2(contents)
                 let to = extractEmail2(contents)
-                // console.log( typeof(to))
-                emails2.push(to)
+                emails2.push(to ? to : '-false')
                 addToTable(to, file.name)
-                // if (emails) {
-                    // finalList = [...finalList, ...emails];
-                    // emails2 = removeDuplicates(emails);
-                    // pre.textContent = emails2.join('\n')
-                    // pre.textContent = newText
-                // }
-                // pre.textContent = emails2.join('\n')
-
-                // console.table(finalList);
             }
         };
         reader.readAsText(file); // or readAsDataURL(file) for images
@@ -101,31 +83,6 @@ fileInput.addEventListener('change', function(event) {
     processFiles(files);
 });
 
-// cleanup.addEventListener('click', e => {
-//     // console.clear()
-//     // emails2 = []
-//     console.table(emails2)
-
-//     // emails2 = removeDuplicates(emails)
-//     for (let i = emails2.length - 1; i >= 0; i--) {
-//     // for (let i = 0; i < emails2.length; i++) {
-//     let yn = myCleanup(emails2[i])
-//         console.log(i, emails2[i], yn)
-//         if (yn) {
-            
-//             emails2.splice(i, 1)
-//         }
-        
-//     }
-//     console.table(emails2)
-
-
-    
-//     pre.textContent = emails2.join('\n')
-//     // console.table(emails2)
-// })
-
 dlBtn.addEventListener('click', e => {
-    // console.log(finalList)
     downloadCSV(emails2)
 })
